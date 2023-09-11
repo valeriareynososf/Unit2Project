a47ec2e3428fc2d2bdbfa4703350bc7faa888c9a
Valeria Reynoso <valeriayearup@gmail.com>
2023-09-11 13:59:40 -0700

    finished

--- js/script.js
+++ js/script.js
@@ -20,30 +20,30 @@ This function will create and insert/append the elements needed to display a "pa
 
 const showPage = (list, page) => {
 
-const startIndex = ( page * 9)- page ;
-const endIndex = page * 9;
+const startIndex = ( page * 9)- 9 ;
+const endIndex = (page * 9) ;
 
 const studentList = document.querySelector('.student-list');
 studentList.innerHTML = '';
 
 for (let i = 0; i < list.length; i++) {
-if (i => startIndex && i < endIndex) {
-
-  const htmlString = `
+if (i >= startIndex && i < endIndex) {
+const person = list[i];
+  const html = `
   <li class="student-item cf">
   <div class="student-details">
-    <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
-    <h3>Ethel Dean</h3>
-    <span class="email">ethel.dean@example.com</span>
+    <img class="avatar" src=${person.picture.large} alt="Profile Picture">
+    <h3>${person.name.title} ${person.name.first} ${person.name.last}</h3>
+    <span class="email">${person.email}</span>
   </div>
   <div class="joined-details">
-    <span class="date">Joined 12-15-2005</span>
+    <span class="date">Joined ${person.registered.date}</span>
   </div>
 </li>
   `;
 
 //   studentList += htmlString;
-studentList.insertAdjacentHTML("afterbegin", htmlString)
+studentList.insertAdjacentHTML("beforeend", html)
 }
 }
 }
@@ -56,7 +56,35 @@ This function will create and insert/append the elements needed for the paginati
 const addPagination = (list ) => {
 const buttons = Math.ceil(list.length/9);
 const buttonList = document.querySelector('.link-list');
+buttonList.innerHTML = '';
+
+for (let i = 1; i <= buttons; i++) {
+const html = `
+<li>
+   <button type="button">${i}</button>
+ </li>
+`;
+buttonList.insertAdjacentHTML("beforeend", html);
+
+ buttonList.querySelector('button').classList.add('active');
+
 }
 
+buttonList.addEventListener("click", (e) => {
+   const activeButton = buttonList.querySelector('.active')
+   const buttonClicked = e.target.closest("button");
+   console.log("buttonClicked", buttonClicked)
+   if (buttonClicked) {
+      activeButton.classList.remove('active');
+      console.log("buttonClicked", buttonClicked)
+      buttonClicked.classList.add('active');
+      showPage(list,  buttons)
+   }
+})
 
+}
+
+console.log(data)
 // Call functions
+addPagination(data);
+showPage(data, 1)
